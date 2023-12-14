8e356cbfc1aba41cd41abe5a582ccbdd559d3590
Peyton Holston <peytonholston18@gmail.com>
2023-12-13 23:39:20 -0600

    Updated navigation styling

--- React/personal-finance-app/src/components/Navigation.js
+++ React/personal-finance-app/src/components/Navigation.js
@@ -2,23 +2,23 @@
 const Navigation = () => {
     return (
         <aside className="sidebar">
-            <div className="logo">Obsidian Finance</div>
+            <div className="sb-top-section">
+            <div className="logo"><h1>Obsidian Finance</h1></div>
             <nav>
                 <ul className="sb-menu">
-                    <div className="sb-item-container">
                         <li className="sb-item"><a href="" className="sb-link">Dashboard</a></li>
                         <li className="sb-item"><a href="" className="sb-link">Spending</a></li>
                         <li className="sb-item"><a href="" className="sb-link">Budgets</a></li>
                         <li className="sb-item"><a href="" className="sb-link">Transactions</a></li>
                         <li className="sb-item"><a href="" className="sb-link">Financial Health</a></li>
                         <li className="sb-item"><a href="" className="sb-link">Statistics</a></li>
-                    </div>
                 </ul>
             </nav>
-            <div className="aside-items">
-                <div className="sb-item-container">
+            </div>
+            <div className="sb-bottom-section">
+                <ul className="sb-menu">
                     <li className="sb-item"><a href="" className="sb-link">Settings</a></li>
-                </div>
+                </ul>
                 <div className="sb-quote">
                     <p>"Being rich is having money; being wealthy is having time."</p>
                     <span>Margaret Bonanno</span>
